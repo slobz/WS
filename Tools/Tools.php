a1b36762d174ee831cbd6d73c90f9d32551a2734
seb <pecoul.s@gmail.com>
2014-11-07 12:08:02 +0100

    AJout de deux fonction pour distinguer la prod du dev

--- Tools/Tools.php
+++ Tools/Tools.php
@@ -8,11 +8,20 @@
 
 class Tools {
 
+    static $url_dev  = 'localhost';
+    static $url_prod = '';
+    
     static function isPostRequest(){
         return $_SERVER['REQUEST_METHOD'] === 'POST';
     }
     
+    static function isProd(){
+        return $_SERVER['HTTP_HOST'] == self::$url_prod;
+    }
     
+    static function isDev(){
+        return $_SERVER['HTTP_HOST'] == self::$url_dev;
+    }
 }
 
 
