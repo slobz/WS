2ab9a253f6acc47b4be2956365319923ed96e70d
Seb <pecoul.s@gmail.com>
2015-01-08 15:39:56 +0100

    Ajout image integre

--- Services/RestaurantService.php
+++ Services/RestaurantService.php
@@ -19,17 +19,23 @@ class RestaurantService extends Service {
     //@override
     public function add($params) {
 
-        $nom = Tools::getValueFromArray($params,'nom');
-        $description = Tools::getValueFromArray($params,'description');
+        $nom = Tools::getValueFromArray($params, 'nom');
+        $description = Tools::getValueFromArray($params, 'description');
         $coordonneeX = Tools::getValueFromArray($params, 'x');
         $coordonneeY = Tools::getValueFromArray($params, 'y');
         $cp = Tools::getValueFromArray($params, 'cp');
         $ville = Tools::getValueFromArray($params, 'ville');
         $rue = Tools::getValueFromArray($params, 'rue');
-        
-        if (!empty($nom) && !empty($description) && !empty($coordonneeX) && !empty($coordonneeY) 
-            && !empty($rue) && !empty($ville) && !empty($cp)) {
-            
+
+        $images = array();
+        $images[] = Tools::getValueFromArray($params, 'img1');
+        $images[] = Tools::getValueFromArray($params, 'img2');
+        $images[] = Tools::getValueFromArray($params, 'img3');
+
+
+
+        if (!empty($nom) && !empty($description) && !empty($coordonneeX) && !empty($coordonneeY) && !empty($rue) && !empty($ville) && !empty($cp)) {
+
             $restaurant = new Restaurant();
             $restaurant->setNom($nom);
             $restaurant->setDescription($description);
@@ -38,28 +44,48 @@ class RestaurantService extends Service {
             $restaurant->setCp($cp);
             $restaurant->setVille($ville);
             $restaurant->setRue($rue);
-            
+
             $this->entityManager->persist($restaurant);
             $this->entityManager->flush();
-            
-              $json = array(
+
+            // AJout des images
+            for ($i = 0; $i < 3; ++$i) {
+                if($images[$i] != null){
+                    
+                    $decode = base64_decode($images[$i]);
+                    $uploadOk = file_put_contents("img/".$i, $decode);
+                   
+                    if(!$uploadOk){
+                      echo json_encode( array(
+                                'error' => true,
+                                'libelleError' => 'Erreur lors de l\'upload'));
+                    }
+                    
+                    $image = new \Entity\IF26\Image();
+                    $image->setName($i);
+                    $image->setRestaurant($restaurant);
+                    $image->setPath("img/".$i);
+                    $this->entityManager->persist($image);
+                    $this->entityManager->flush();
+                }
+            }
+
+            $json = array(
                 'error' => false
             );
-            
         } else {
             header("HTTP/1.1 400 BAD REQUEST");
         }
-        
+
         return json_encode($json);
-        
     }
 
     //@override
     public function get($params) {
 
         $repo = $this->entityManager->getRepository(Service::ENTITE_RESTAURANT);
-        $id = Tools::getValueFromArray($params,'id');
-        
+        $id = Tools::getValueFromArray($params, 'id');
+
         if (!empty($id)) {
 
             $restaurant = $repo->find($id);
@@ -68,15 +94,14 @@ class RestaurantService extends Service {
                 "restaurant" => $restaurant->toArray()
             );
         } else {
-            
+
             $restaurants = $repo->findAll();
             foreach ($restaurants as $restaurant) {
                 $tableauRestaurants[] = $restaurant->toArray();
             }
-            
-            
+
+
             $json = array('restaurants' => $tableauRestaurants);
-            
         }
         return json_encode($json);
     }
